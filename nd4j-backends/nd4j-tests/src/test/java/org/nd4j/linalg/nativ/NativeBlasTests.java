f12870833e7e6d540647508bdc187dae220a3eb8
raver119 <raver119@gmail.com>
2017-11-27 20:18:02 +0300

    test + debug/verbose setters

--- nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/nativ/NativeBlasTests.java
+++ nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/nativ/NativeBlasTests.java
@@ -1,11 +1,16 @@
 package org.nd4j.linalg.nativ;
 
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.junit.Before;
 import org.junit.Test;
 import org.nd4j.linalg.BaseNd4jTest;
+import org.nd4j.linalg.api.ops.DynamicCustomOp;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.factory.Nd4jBackend;
 import org.nd4j.nativeblas.NativeOps;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * This class contains tests related to javacpp presets and their libnd4j integration
@@ -18,13 +23,33 @@ public class NativeBlasTests extends BaseNd4jTest {
         super(backend);
     }
 
+    @Before
+    public void setUp() {
+        //Nd4j.getExecutioner()
+    }
+
     @Test
     public void testBlasGemm1() {
+
         // we're skipping blas here
         if (Nd4j.getExecutioner().getClass().getSimpleName().toLowerCase().contains("cuda"))
             return;
 
+        val A = Nd4j.linspace(1, 9, 9).reshape('c', 3, 3);
+        val B = Nd4j.linspace(1, 9, 9).reshape('c', 3, 3);
 
+        val exp = A.mmul(B);
+
+        val res = Nd4j.create(new int[] {3, 3}, 'c');
+
+        val matmul = DynamicCustomOp.builder("matmul")
+                .addInputs(A, B)
+                .addOutputs(res)
+                .build();
+
+        Nd4j.getExecutioner().exec(matmul);
+
+        assertEquals(exp, res);
     }
 
     @Override
